6db3ea0ca059efbda25b0595aa85bd7e6eb808dd
SANTA\Vadim <vadim.albarov@softangible.com>
2017-12-21 00:08:40 +0200

    *implement base functionality

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
         documentHeight: 0,
         documentWidth: 0,
         body: 'body',
-        window: window
+        window: window,
+        rullerList: {}
     };
 
     let Ruller = {
@@ -24,7 +25,8 @@
     let event = {
         create: 'mousedown.ruller',
         drow: 'mousemove.ruller',
-        fix: 'mouseup.ruller'
+        fix: 'mouseup.ruller',
+        keypress: 'keypress'
     }
 
     let count = 0;
@@ -37,13 +39,23 @@
 
         Ruller.overlay = $('<div/>', { class: Ruller.overlay }).appendTo(Global.body).css({ width: Global.documentWidth, height: Global.documentHeight });
         run();
+        bindQuit();
     };
 
     function run() {
-        Global.window.on(event.create, function(e) {
-            var ruller = new ChromeRuller(e, count++);
-        });
+        Global.window.on(event.create, (e) => Global.rullerList[count] = new ChromeRuller(e, count++));
     };
+
+    function bindQuit() {
+        Global.window.on(event.keypress, function(e) {
+            if (e.keyCode == 17) {
+                for (const key in Global.rullerList) {
+                    if (Global.rullerList.hasOwnProperty(key)) Global.rullerList[key].remove();
+                }
+            }
+        });
+    }
+
 
     let ChromeRuller = function(e, count) {
         this.count = count;
@@ -51,6 +63,7 @@
         this.startY = e.clientY;
         this.endX = 0;
         this.endY = 0;
+        this.forceStay = e.ctrlKey;
 
         this.wrapper = $('<div/>', { class: Ruller.wrapper }).appendTo(Ruller.overlay);
         this.selection = $('<div/>', { class: Ruller.main }).appendTo(this.wrapper);
@@ -65,6 +78,7 @@
 
         Global.window.on(event.drow + this.count, this.drow.bind(this));
         Global.window.on(event.fix + this.count, this.fix.bind(this));
+        Global.window.on(event.create + this.count, this.checkForStay.bind(this));
     };
 
     ChromeRuller.prototype.drow = function(e) {
@@ -79,17 +93,23 @@
         this.wrapper.css({ left: x3, top: y3, width: width, height: height });
         this.heightLabel.html(height);
         this.widthLabel.html(width);
-    }
+    };
 
     ChromeRuller.prototype.fix = function(e) {
         Global.window.off(event.drow + this.count);
-        if (!e.ctrlKey) this.remove();
+    };
+
+    ChromeRuller.prototype.checkForStay = function() {
+        if (!this.forceStay) this.remove();
     }
 
     ChromeRuller.prototype.remove = function() {
+        Global.window.off(event.create + this.count);
         Global.window.off(event.drow + this.count);
         Global.window.off(event.fix + this.count);
+        this.wrapper.remove();
+        delete Global.rullerList[this.count];
     };
 
     $(init);
-}(jQueryChromeRuller))
+}(jQueryChromeRuller));
